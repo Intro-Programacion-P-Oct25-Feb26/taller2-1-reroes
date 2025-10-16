f6b34a4bc3efd44844d2ec020c89df489d714cdd
René Elizalde <reroes799@gmail.com>
2025-10-16 12:34:42 -0500

    primer cambio con dirección de la U

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -15,6 +15,7 @@ public class Proyecto01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Dirección de UTPL: Calle Paris S/N");
     }
     
 }
